c335e3c634e8e0389767d877ba47ae20da62f881
Junho Ryu <jryu@users.noreply.github.com>
2015-11-21 17:26:18 -0800

    Use autotools

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-rm a.out a.png
-cc $(pkg-config --cflags cairo) calendar.c $(pkg-config --libs cairo) &&
-	./a.out < special_days &&
-	xdg-open a.png
+make calendar && ./calendar < special_days && xdg-open a.png
