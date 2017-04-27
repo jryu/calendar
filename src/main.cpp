7a820892d57e5d4301424b918564043b637a2ea7
Junho Ryu <jryu@users.noreply.github.com>
2017-04-26 22:01:53 -0700

    Use Pango to render text.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include <cairo.h>
+#include <pango/pangocairo.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 
 #include "holidays.h"
 
-#define CELL_WIDTH 20
-#define CELL_HEIGHT 20
-#define FONT_SIZE 10
+#define CELL_WIDTH 40
+#define CELL_HEIGHT 40
+#define FONT_SIZE 20
 #define MAX_YEAR 30
+
+#define RGB_SPECIAL_DAY 63,87,101
+#define RGB_SUNDAY 189,212,222
+#define RGB_HOLIDAY 255,83,13
+#define RGB_COMMON_DAY 239,239,239
+#define RGB_LINE 255,255,255
+#define RGB_NUMBER_BRIGHT 255,255,255
+#define RGB_NUMBER_DARK 43,58,66
 
 int special_days[10][2];
 
@@ -47,6 +56,39 @@ time_t get_first_day_of_year_in_sec(int year) {
 	return mktime(&timeinfo);
 }
 
+void draw_text(cairo_t *cr, int x, int y, const char* text) {
+	int width, height;
+	PangoLayout *layout = pango_cairo_create_layout(cr);
+	PangoFontDescription *font_description =
+		pango_font_description_from_string("Source Code Pro Bold");
+	pango_font_description_set_absolute_size(font_description,
+			FONT_SIZE * PANGO_SCALE);
+
+	pango_layout_set_font_description (layout, font_description);
+	pango_font_description_free(font_description);
+
+	pango_layout_set_text(layout, text, -1);
+
+	pango_layout_get_size(layout, &width, &height);
+	cairo_move_to(cr,
+			(x + 0.5) * CELL_WIDTH - ((double)width / PANGO_SCALE) / 2,
+			(y + 0.5) * CELL_HEIGHT - ((double)height / PANGO_SCALE) / 2);
+	pango_cairo_show_layout(cr, layout);
+
+	g_object_unref(layout);
+}
+
+
+double cairo_color(int color) {
+	return (double) color / 255;
+}
+
+
+void set_rgb(cairo_t *cr, int r, int g, int b) {
+	cairo_set_source_rgb(cr, cairo_color(r), cairo_color(g), cairo_color(b));
+}
+
+
 void year(cairo_t *cr, int y, int year) {
 	int i;
 	char buf[4];
@@ -60,36 +102,37 @@ void year(cairo_t *cr, int y, int year) {
 				i * CELL_WIDTH, y * CELL_HEIGHT,
 				CELL_WIDTH, CELL_HEIGHT);
 		if (is_special_day(timeinfo)) {
-			cairo_set_source_rgb(cr, 0, 0, 1);
+			set_rgb(cr, RGB_SPECIAL_DAY);
 			cairo_fill(cr);
 			filled = 1;
 		} else if (timeinfo->tm_wday == 0) {
-			cairo_set_source_rgb(cr, 0, 0, 0);
+			set_rgb(cr, RGB_SUNDAY);
 			cairo_fill(cr);
-			filled = 1;
 		} else if (is_holiday(t)) {
-			cairo_set_source_rgb(cr, 1, 0, 0);
+			set_rgb(cr, RGB_HOLIDAY);
 			cairo_fill(cr);
 			filled = 1;
+		} else {
+			set_rgb(cr, RGB_COMMON_DAY);
+			cairo_fill(cr);
 		}
 		timeinfo = localtime(&t);
 
 		cairo_rectangle(cr,
 				i * CELL_WIDTH, y * CELL_HEIGHT,
 				CELL_WIDTH, CELL_HEIGHT);
-		cairo_set_source_rgb(cr, 0, 0, 0);
+		set_rgb(cr, RGB_LINE);
 		cairo_stroke(cr);
 
 		// Label
 		if (filled) {
-			cairo_set_source_rgb(cr, 1, 1, 1);
+			set_rgb(cr, RGB_NUMBER_BRIGHT);
+		} else {
+			set_rgb(cr, RGB_NUMBER_DARK);
 		}
 		if (timeinfo->tm_mday == 1) {
 			sprintf(buf, "%d", timeinfo->tm_mon + 1);
-			cairo_move_to(cr,
-					i * CELL_WIDTH + CELL_WIDTH / 2 - FONT_SIZE / 3,
-					y * CELL_HEIGHT + FONT_SIZE + (CELL_HEIGHT - FONT_SIZE) / 2);
-			cairo_show_text(cr, buf);
+			draw_text(cr, i, y, buf);
 		}
 
 		timeinfo = get_next_day(&t);
@@ -108,11 +151,7 @@ int main(int argc, char *argv[])
 			366 * CELL_WIDTH, MAX_YEAR * CELL_HEIGHT);
 	cairo_t *cr = cairo_create(surface);
 
-	cairo_select_font_face(cr, "serif", CAIRO_FONT_SLANT_NORMAL,
-			CAIRO_FONT_WEIGHT_BOLD);
-	cairo_set_font_size(cr, FONT_SIZE);
-
-	cairo_set_line_width(cr, 1);
+	cairo_set_line_width(cr, 3);
 
 	int this_year = get_this_year();
 	for (i = 0; i < MAX_YEAR; i++) {
